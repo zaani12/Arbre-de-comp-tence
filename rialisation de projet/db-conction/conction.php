006a753c06a54b613ae6fb95c3909f7e4507e2b5
zaani12 <zaani.hamza.solicode.com>
2023-10-15 11:56:21 +0100

    set up envirenment

---
+++ rialisation de projet/db-conction/conction.php
@@ -0,0 +1,24 @@
+<?php
+class Connect
+{
+  const SERVERNAME = "localhost";
+  const DATABASNAME = "sprint1db";
+  const USERNAME = "root";
+  const PASSWORD = "";
+
+
+  public static function DB()
+  {
+    try {
+
+      $conn = new PDO("mysql:host=" . self::SERVERNAME . ";dbname=" . self::DATABASNAME, self::USERNAME, self::PASSWORD);
+      // set the PDO error mode to exception
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      // echo "Connected successfully";
+      return $conn;
+    } catch (PDOException $e) {
+      echo "Connection failed: " . $e->getMessage();
+    }
+  }
+}
+?>
